5843e8c4eaf204c80762f404969f87a05c368b49
zhenyun.su <zhenyun.su@qq.com>
2019-11-12 15:37:16 +0800

    执行子系统 - 类加载

--- 1JDK语言/实例代码/java_demo/src/jvm/Test.java
+++ 1JDK语言/实例代码/java_demo/src/jvm/Test.java
@@ -9,7 +9,29 @@ import jvm.classs.ByteCode;
  */
 
 public class Test {
+    final static String s1= "zhenyun.su";
+    final static String s2 = "zhenyun.su";
+    final static String s3 =  new String("zhenyun.su");
+
+    public Test() {
+//        s1 = "zhenyun.su";
+//        s2 = "zhenyun.su";
+//        s3 = new String("zhenyun.su");
+    }
+
+//    String s3 = new String("zhenyun.su");
+
     public static void main(String[] args) {
+//        Test test = new Test();
+        String s1 = "zhenyun.su";
+        String s2 = "zhenyun.su";
+        String s3 = new String("zhenyun.su");
+
+        System.out.println(Test.s1==Test.s2);
+        System.out.println(s1.equals(s2));
+        System.out.println(Test.s1==Test.s3);
+        System.out.println(s1.equals(s3));
+
         ByteCode byteCode = new ByteCode();
         byteCode.add();
     }
